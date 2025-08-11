85879ba6ebaaef2351457cabdc7e1bd71f3f09f2
MyEggsBR <jr_info@hotmail.com>
2025-08-11 17:55:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -226,5 +226,5 @@ def index():
     return html_content
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 55555))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port, debug=False)
